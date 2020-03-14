e4b1353da4874e411e5a06d3331f4e4487db02b8
Unknown <wolfj@post.bgu.ac.il>
2020-03-14 19:32:23 +0200

    tidy up

--- grammar.js
+++ grammar.js
@@ -2,7 +2,6 @@
 const alpha = /[^\s0-9;.,`"'|<=>\\\[\]{}\uFEFF\u2060\u200B\u00A0]|\\u[0-9a-fA-F]{4}|\\u\{[0-9a-fA-F]+\}/
 const alpha_numeric = /[^\s;.,`"'|<=>\\\[\]{}\uFEFF\u2060\u200B\u00A0]|\\u[0-9a-fA-F]{4}|\\u\{[0-9a-fA-F]+\}/
 const string_regex = /[^;`|<=>\\\[\]{}\uFEFF\u2060\u200B\u00A0]|\\u[0-9a-fA-F]{4}|\\u\{[0-9a-fA-F]+\}/
-//const comment_block = /<\*(.|\n)*\*>/
 
 const PREC = {
   COMMENT: 1, // Prefer comments over regexes
@@ -83,7 +82,6 @@ module.exports = grammar({
       ),
 
       //https://stackoverflow.com/questions/13014947/regex-to-match-a-c-style-multiline-comment
-      //comment: $ => token(prec(PREC.COMMENT, choice(
       comment: $ => prec(PREC.COMMENT, choice(
         seq('<--', /.*/), 
         $._comment_block
